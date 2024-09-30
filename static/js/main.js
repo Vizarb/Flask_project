3e85bf3f4d1dfb811ab57d80c4afc0ed0eb78207
Vizarb <zivbarr666@gmail.com>
2024-10-01 00:27:32 +0300

    for upload to render relly2 maybe bad1

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-// Initialize all scripts
-document.addEventListener('DOMContentLoaded', () => {
-    const toastElList = [].slice.call(document.querySelectorAll('.toast'));
-    const toastList = toastElList.map(function (toastEl) {
-        return new bootstrap.Toast(toastEl);
-    });
-});
-
-// Include other scripts here
